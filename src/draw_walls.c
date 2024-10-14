cafc1e70601228173debc16d40c89c6f5a483461
pik-kak <karokauh@gmail.com>
2024-10-14 10:32:20 +0300

    Started working on textures

--- src/draw_walls.c
+++ src/draw_walls.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   draw_walls.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 10:59:45 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/10/10 15:52:02 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/10/14 10:27:47 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../includes/cub3d_bonus.h"
 
 #include "../includes/cub3d_bonus.h"
 
@@ -21,6 +23,32 @@ void	calculate_measurements(t_data *data, int wall_height, int *start, int *end)
 	if (*end >= data->s_height)
 		*end = data->s_height - 1;
 }
+//Util function to get color from an image
+/*uint32_t get_image_color(mlx_image_t *image, int tex_x, int tex_y)
+{
+	int pixel_index = (tex_y * image->width + tex_x) * 4;// Assuming each pixel is 4 bytes (RGBA)
+
+	uint8_t r = image->pixels[pixel_index];	// Get the pixel color (adjust channels if needed)
+	uint8_t g = image->pixels[pixel_index + 1];
+	uint8_t b = image->pixels[pixel_index + 2];
+	uint8_t a = image->pixels[pixel_index + 3];
+	return (r << 24 | g << 16 | b << 8 | a);// Combine the color channels into a single 32-bit value
+}
+
+void	draw_texture(t_data *data, int ray, int start, int wall_height, mlx_image_t *image, int i)
+{
+	int tex_y; // Texture Y coordinate
+	static double step;
+	double tex_pos;
+
+	step = 1.0 * image->height / wall_height;// Calculate the step size for the texture mapping
+	tex_pos = (start - data->s_height / 2 + wall_height / 2) * step;// Calculate the initial texture Y coordinate
+	tex_y = (int)tex_pos & (image->height - 1); // Clamp tex_y to image height
+	tex_pos += step;
+	uint32_t color = get_image_color(image, data->tex_x, tex_y);	// Get the color from the image
+	if (pixel_ok(data, ray, i))
+		mlx_put_pixel(data->image, ray, i, color); // Put the color on screen
+}*/
 
 void draw_walls(t_data *data, int ray, int wall_height)
 {
@@ -32,21 +60,12 @@ void draw_walls(t_data *data, int ray, int wall_height)
 	calculate_measurements(data, wall_height, &start, &end);
 	while (i < data->s_height)
 	{
-		//if (i < start)
-		//{
-		//	if (pixel_ok(data, ray, i))
-		//		mlx_put_pixel(data->image, ray, i, COL_BLUE);
-		//}
 		if (i >= start && i <= end)
 		{
+			//draw_texture(data, ray, start, wall_height, data->walls->no, i);
 			if (pixel_ok(data, ray, i))
 				mlx_put_pixel(data->image, ray, i, COL_FLAMINGO);
 		}
-		//else
-		//{
-		//	if (pixel_ok(data, ray, i))
-		//		mlx_put_pixel(data->image, ray, i, COL_BLUE);
-		//}
 		i++;
 	}
 }
--- src/textures.c
+++ src/textures.c
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   textures.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 15:48:31 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/10/10 15:50:11 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/10/14 10:25:23 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*void	get_textures(t_data *data)
+#include "../includes/cub3d_bonus.h"
+
+void	get_textures(t_data *data)
 {
-	data->textrs = ft_calloc(1, sizeof(struct s_textures));
-	data->textrs->floor = mlx_load_png("./textures/grass.png");
-	data->textrs->wall = mlx_load_png("./textures/hedge.png");
-	data->textrs->collec = mlx_load_png("./textures/mushrooms.png");
-	data->textrs->player = mlx_load_png("./textures/pippin.png");
-	data->textrs->exit = mlx_load_png("./textures/hole.png");
+	data->txtrs = ft_calloc(1, sizeof(struct s_textures));
+	data->txtrs->no = mlx_load_png("./textures/wall_txtr.png");//data->scene.no);
+	data->txtrs->so = mlx_load_png("./textures/wall_txtr.png");//data->scene.so);
+	data->txtrs->ea = mlx_load_png("./textures/wall_txtr.png");//data->scene.ea);
+	data->txtrs->we = mlx_load_png("./textures/wall_txtr.png");//data->scene.we);
+	get_images(data);
 }
 
 void	get_images(t_data *data)
 {
-	data->imag = ft_calloc(1, sizeof(t_images));
-	data->imag->floor = mlx_texture_to_image(data->mlx, data->textrs->floor);
-	data->imag->wall = mlx_texture_to_image(data->mlx, data->textrs->wall);
-	data->imag->collec = mlx_texture_to_image(data->mlx, data->textrs->collec);
-	data->imag->player = mlx_texture_to_image(data->mlx, data->textrs->player);
-	data->imag->exit = mlx_texture_to_image(data->mlx, data->textrs->exit);
-	mlx_delete_texture(data->textrs->floor);
-	mlx_delete_texture(data->textrs->wall);
-	mlx_delete_texture(data->textrs->collec);
-	mlx_delete_texture(data->textrs->player);
-	mlx_delete_texture(data->textrs->exit);
-}*/
+	data->walls = ft_calloc(1, sizeof(t_walls));
+	data->walls->no = mlx_texture_to_image(data->m, data->txtrs->no);
+	data->walls->so = mlx_texture_to_image(data->m, data->txtrs->so);
+	data->walls->ea = mlx_texture_to_image(data->m, data->txtrs->ea);
+	data->walls->we = mlx_texture_to_image(data->m, data->txtrs->we);
+	mlx_delete_texture(data->txtrs->no);
+	mlx_delete_texture(data->txtrs->so);
+	mlx_delete_texture(data->txtrs->ea);
+	mlx_delete_texture(data->txtrs->we);
+}
