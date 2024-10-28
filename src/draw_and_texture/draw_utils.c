e2392c868e37341007d43600dcbe6a4424672920
pik-kak <karokauh@gmail.com>
2024-10-28 22:17:28 +0200

    Corrected norm

--- src/draw_and_texture/draw_utils.c
+++ src/draw_and_texture/draw_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsaari <tsaari@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:47:29 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/10/25 08:23:46 by tsaari           ###   ########.fr       */
+/*   Updated: 2024/10/28 21:05:40 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /*
 Calculates the starting and ending points of the wall slice
 */
-void calculate_measurements(t_data *data, int wall_height, int *start, int *end)
+void	calculate_measurements(t_data *data, int wall_height, int *start, int *end)
 {
 	*start = -(wall_height / 2) + (data->s_height / 2);
 	if (*start < 0)
@@ -25,32 +25,38 @@ void calculate_measurements(t_data *data, int wall_height, int *start, int *end)
 		*end = data->s_height - 1;
 }
 
-float calculate_opacity(int ray, int nbr_of_rays, int start, int end)
+float	calculate_opacity(int ray, int nbr_of_rays, int start, int end)
 {
-	float distance_from_center;
-	float opac;
+	float	distance_from_center;
+	float	opac;
 
 	distance_from_center = fabs((float)nbr_of_rays / 2 - (float)ray);
 	opac = 1 - (distance_from_center / 1000);
-	return opac;
+	return (opac);
 }
 
-int squared_distance(int x1, int y1, int x2, int y2)
+int	squared_distance(int x1, int y1, int x2, int y2)
 {
-	int dx = x1 - x2;
-	int dy = y1 - y2;
+	int	dx;
+	int	dy;
 
-	return (dx * dx) + (dy * dy);
+	dx = x1 - x2;
+	dy = y1 - y2;
+	return ((dx * dx) + (dy * dy));
 }
 
 // Function to adjust brightness
-uint32_t darken_color(uint32_t color, double factor) 
+uint32_t	darken_color(uint32_t color, double factor)
 {
-	uint8_t r = (color >> 24) & 0xFF;
-	uint8_t g = (color >> 16) & 0xFF;
-	uint8_t b = (color >> 8) & 0xFF;
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+
+	r = (color >> 24) & 0xFF;
+	g = (color >> 16) & 0xFF;
+	b = (color >> 8) & 0xFF;
 	r = (uint8_t)(r * factor);
 	g = (uint8_t)(g * factor);
 	b = (uint8_t)(b * factor);
-	return (r << 24) | (g << 16) | (b << 8) | 0xFF;
+	return ((r << 24) | (g << 16) | (b << 8) | 0xFF);
 }
