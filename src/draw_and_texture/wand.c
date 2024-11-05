e45b7c7e471e940fd4f676254be03f34b99f4ec2
Timo Saari <tsaari@c2r3p11.hive.fi>
2024-11-05 16:52:53 +0200

    added last changes

--- src/draw_and_texture/wand.c
+++ src/draw_and_texture/wand.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wand.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: tsaari <tsaari@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 17:47:48 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/11/04 16:54:35 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:06:07 by tsaari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,15 @@ void	update_wand(t_data *data)
 
 	mlx_delete_image(data->m, data->wand);
 	if (data->scene.wand_pos == 1)
-		wand_text = mlx_load_png("./textures/wand_2.png");
+		wand_text = mlx_load_png("./textures/testwand2.png");
 	else if (data->scene.wand_pos == 2)
-		wand_text = mlx_load_png("./textures/wand_3.png");
+		wand_text = mlx_load_png("./textures/wand_spell1.png");
+	else if (data->scene.wand_pos == 3)
+		wand_text = mlx_load_png("./textures/wand_spell2.png");
 	if (!wand_text)
 		ft_free_data_and_error(data, "error loading wand texture");
 	data->wand = mlx_texture_to_image(data->m, wand_text);
 	mlx_delete_texture(wand_text);
-	if (data->scene.wand_pos == 1)
-		mlx_image_to_window(data->m, data->wand, WIDTH / 2, HEIGHT - 200);
-	else if (data->scene.wand_pos == 2)
-		mlx_image_to_window(data->m, data->wand, WIDTH / 2, HEIGHT - 200);
-	mlx_set_instance_depth(&data->wand->instances[0], 3);
+	mlx_image_to_window(data->m, data->wand, WIDTH / 2 - 200, HEIGHT - 800);
+	mlx_set_instance_depth(&data->wand->instances[0], 4);
 }
